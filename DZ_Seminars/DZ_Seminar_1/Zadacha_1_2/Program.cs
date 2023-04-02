890db369903bc325b97e4c2d98c9912b8e89edce
Maryana <mar1610alex@gmail.com>
2023-04-02 22:48:08 +0300

    на вход два числа, на выход большее

---
+++ DZ_Seminars/DZ_Seminar_1/Zadacha_1_2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//           a = 5; b = 7 -> max = 7
+//           a = 2 b = 10 -> max = 10
+//           a = -9 b = -3 -> max = -3
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+bool Seta = number1 > number2;
+bool Setb = number1 == number2;
+bool Setc = number1 < number2;
+
+if (Seta)
+{
+    Console.WriteLine($"max = {number1}");
+}
+else if (Setb)
+{
+    Console.WriteLine($"Числа {number1} и {number2} равны");
+}
+else 
+{
+  Console.WriteLine($"max = {number2}");  
+}
