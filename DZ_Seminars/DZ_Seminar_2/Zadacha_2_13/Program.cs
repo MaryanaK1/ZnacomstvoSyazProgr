d8d71452543e51e962c6cf9c3a5ff64d1766bd18
Maryana <mar1610alex@gmail.com>
2023-04-03 22:40:12 +0300

    выход третья цифра заданного числа

---
+++ DZ_Seminars/DZ_Seminar_2/Zadacha_2_13/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//            645 -> 5
+//            78 -> третьей цифры нет
+//            32679 -> 6
+int num = 0;
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 0) { num = number * (-1); }
+else num = number;
+
+if (num < 100)
+{
+    Console.WriteLine($"Третьей цифры в числе {number} нет ");
+}
+else
+{
+    while (num > 999)
+    {
+        num = num / 10;
+    }
+    Console.WriteLine($"Третья цифра числа {number} это {num % 10} ");
+}
