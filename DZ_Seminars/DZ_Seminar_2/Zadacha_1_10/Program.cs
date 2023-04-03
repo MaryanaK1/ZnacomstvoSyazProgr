af0c60d7bbf34a7e775d8f6de9d0ebf0f18da778
Maryana <mar1610alex@gmail.com>
2023-04-03 22:27:59 +0300

    на вход трехзн число, на выход вторая цифра

---
+++ DZ_Seminars/DZ_Seminar_2/Zadacha_1_10/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//            456 -> 5
+//            782 -> 8
+//            918 -> 1
+int num = 0;
+Console.WriteLine("Введите целое трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 0) { num = number * (-1); }
+else
+    num = number;
+
+if (num >= 100 && num <= 999)
+{
+    int firstDigit = num / 10;
+    int thirdDigit = firstDigit % 10;
+    num = thirdDigit;
+    Console.WriteLine($"Вторая цифра числа {number} это {num} ");
+}
+else
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+    Console.WriteLine("Введите трехзначное число");
+}
