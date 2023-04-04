d036f621eae611dfb8d0d42abe0ed76d206fb710
Maryana <mar1610alex@gmail.com>
2023-04-04 23:20:10 +0300

    коорд двух точек - расст м/у ними в 3D

---
+++ DZ_Seminars/DZ_Seminar_3/Zadacha2_21/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Задача 21
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53   */
+Console.WriteLine("Введите координаты точки X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки Z: ");
+int z = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки X1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки Y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки Z1: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+double res = Math.Sqrt(Math.Pow(x1 - x, 2) + Math.Pow(y1 - y, 2) + Math.Pow(z1 - z, 2));
+
+Console.WriteLine($"Расстояние между точками = {Math.Round(res,2)}");
+
