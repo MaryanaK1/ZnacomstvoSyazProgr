fe7cc05f80d1b2cd9e1e7933ee4e547449e778ab
Maryana <mar1610alex@gmail.com>
2023-04-06 19:56:20 +0300

    число- сумма цифр

---
+++ DZ_Seminars/DZ_Seminar_4/Zadacha2_27/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+              452 -> 11
+              82 -> 10
+              9012 -> 12     */
+int ReadNumber(string massage)
+{
+    Console.Write(massage);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void SumDigits(int num)
+{
+    int number = num;
+    if (number < 0) { number = number * (-1); }
+    else num = number;
+    int sum = 0;
+    int digit = 0;
+    while (number > 0)
+    {
+        digit = number % 10;
+        sum = sum + digit;
+        number = number / 10;
+    }
+    Console.Write($"{num} -> {sum}");
+}
+int N = ReadNumber("Введите число: ");
+SumDigits(N);
