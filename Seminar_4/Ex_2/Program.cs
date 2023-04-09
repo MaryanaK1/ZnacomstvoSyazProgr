3a930791c63b923d3060397c0c7701587fdbc0d8
Maryana <mar1610alex@gmail.com>
2023-04-09 22:32:58 +0300

    на вход число - колво цифр в числе

---
+++ Seminar_4/Ex_2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 26. Напишите программу, которая принимает на вход число и выдает количество цифр в числе.
+//            456-> 3
+Console.WriteLine("Введите число А: ");
+int inputNumber = GetInputNumber();
+int amountOfDigits = CountDigits(inputNumber);
+Console.WriteLine(amountOfDigits);
+
+int GetInputNumber()
+{
+    string input = Console.ReadLine();
+    int result = int.Parse(input);
+    return result;
+}
+int CountDigits(int a)
+{
+    int digitsCounter = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        digitsCounter++;
+    }
+    return digitsCounter;
+}
