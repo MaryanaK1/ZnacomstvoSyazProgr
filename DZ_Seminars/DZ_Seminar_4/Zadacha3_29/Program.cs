7b3596dddf0378f8f89a47da63ee055f03ed2b16
Maryana <mar1610alex@gmail.com>
2023-04-09 23:46:27 +0300

    длина массива, мин, макс значение- вывод массива

---
+++ DZ_Seminars/DZ_Seminar_4/Zadacha3_29/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Задача 29: Напишите программу, которая приниматет на вход длину массива, минимальное значение массива
+ и максимальное значение массива, создаёт массив с заданными параметрами и выводит значения на экран
+
+5, 1, 19 -> [1, 2, 5, 7, 19]
+3, 1, 33 -> [6, 1, 33]     */
+
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите длину массива: ");
+            int length = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите минимальное значение массива: ");
+            int minValue = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите максимальное значение массива: ");
+            int maxValue = int.Parse(Console.ReadLine());
+
+            Random random = new Random();
+            int[] array = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                array[i] = random.Next(minValue, maxValue + 1);
+            }
+
+            Console.WriteLine($"Сгенерированный массив: [{string.Join(", ", array)}]");
+        }
+    }
