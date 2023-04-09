a6ee0c7152b676596b94662b2cfaf6669767a385
Maryana <mar1610alex@gmail.com>
2023-04-09 23:33:54 +0300

    на вход N-произведение от 1 до N

---
+++ Seminar_4/Ex_3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 28. Напишите программу, которая принимает на вход число N
+//            и выдает произведение чисел от 1 до N.
+//         4 -> 24        5 -> 120
+
+Console.WriteLine("Введите число: ");
+int inputInt = GetIntInput();
+Console.WriteLine(MultNumb(inputInt));
+
+int GetIntInput()
+{
+    string input = Console.ReadLine();
+    int result = int.Parse(input);
+    return result;
+}
+
+int MultNumb(int number)
+{
+    int sum = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        sum*= i;
+    }
+    return sum;
+}
