71ad07b6c92b585c391a038dbf4968593a03d76b
Maryana <mar1610alex@gmail.com>
2023-04-09 21:26:53 +0300

    на вход число A-сумма от 1 до A

---
+++ Seminar_4/Ex_1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, кот. на вход принимает число (А) и выдает сумму чисел от 1 до А.
+// 7->28
+// 4->10
+// 8->36
+
+Console.WriteLine("Введте число А: "); 
+int inputInt = GetIntInput();
+int sum = sumFrom1toA(inputInt);
+Console.WriteLine(sum);
+
+int GetIntInput()
+{
+    string input = Console.ReadLine();
+    int result = int.Parse(input);
+    return result;
+}
+
+int sumFrom1toA(int a)
+{
+    int sum = 0;
+    for (int i = 1; i <= a; i++)
+    {
+        sum +=i;  // sum = sum + i
+    }
+    return sum;
+}
