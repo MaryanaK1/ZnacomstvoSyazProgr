59812364b3eea543f30b9ea5430b59de6f7439e8
Maryana <mar1610alex@gmail.com>
2023-04-10 22:17:15 +0300

    массив случ чисел - сумма эл-тов на четн позициях

---
+++ DZ_Seminars/DZ_Seminar_5/Zadacha2_36/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+              [3, 7, 23, 12] -> 19
+              [-4, -6, 89, 6] -> 0           */
+int [] GetRandomArray(nint size, int leftRange, int rightRange)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(leftRange, rightRange+1);
+    }
+    return array;
+}
+
+const int size = 10;
+const int leftRange = -5;
+const int rightRange = 5;
+int[] massive = GetRandomArray (size, leftRange, rightRange);  // int[] massive = GetRandomArray (10, -5, 5);
+//Console.WriteLine(string.Join(", ", massive));
+
+int sumNotEven(int[] array)
+{
+    int sum = 0;   //сумма элементов, стоящих на нечётных позициях
+    for (int i = 0; i < massive.Length; i++)
+    {
+        if (i % 2 != 0 )  
+        {
+            sum += massive[i];    
+        }
+    }
+    return sum;
+}
+Console.WriteLine((string.Join(", ", massive )) + " -> " + $"{sumNotEven(array: massive)}");
