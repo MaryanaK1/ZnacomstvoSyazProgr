271fdefb981d8b49e7978f94beaab6d6e58a0cf0
Maryana <mar1610alex@gmail.com>
2023-04-14 22:35:30 +0300

    ввод Мчисел- сколько 0> введено

---
+++ DZ_Seminars/DZ_Seminar_6/Zadacha1_41/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3 */
+
+// Ввод чисел останавливается при помощи ввода стоп-слова "stop"
+int number;
+int count = 0;
+string answer;
+string answerStop = "stop";
+while (true)
+{
+    Console.Write("Введите число или словo stop : ");
+    answer = Console.ReadLine();
+    if (answer == answerStop)
+    {
+        break;
+    }
+    number = Convert.ToInt32(answer);
+    if (number > 0) count++;
+}
+Console.WriteLine(count);
